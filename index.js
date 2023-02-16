3321016eb482926a8d804a25fac1c04331b9ce9b
farosFreed <jessica.divers@gmail.com>
2023-02-16 16:14:49 -0700

    setup mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 // enable body parsing
 const bodyParser = require("body-parser");
 // enable dns lookup
@@ -9,6 +10,13 @@ const dns = require("node:dns");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+
+// setup mongoose
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(cors());
 
@@ -27,6 +35,35 @@ app.get("/api/hello", function (req, res) {
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/api/shorturl", bodyParser.urlencoded({ extended: false }));
 
+// setup url pair mongoose scheme
+let urlSchema = new mongoose.Schema({
+  original_url: {
+    type: String,
+    required: true,
+  },
+  short_url: {
+    type: Number,
+    required: true,
+  },
+});
+
+let UrlPair = mongoose.model("urlPair", urlSchema);
+
+const createAndSaveUrl = (original, short, done) => {
+  let newpair = new UrlPair({
+    original_url: original, //req.body.original_url,
+    short_url: short, //req.body.short_url,
+  });
+
+  newpair.save((err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    done(null, data);
+  });
+};
+
+// save url pair to mongoose database
 app.post("/api/shorturl", (req, res) => {
   let urlWOprotocol = req.body.original_url.match(/\/\/([a-z, A-Z, ., \/]*)/);
   console.log(urlWOprotocol);
@@ -36,6 +73,34 @@ app.post("/api/shorturl", (req, res) => {
         error: "invalid url",
       });
     } else {
+      // save to mongoose
+      // in case of incorrect function use wait timeout then respond
+      let t = setTimeout(() => {
+        next({ message: "timeout" });
+      }, TIMEOUT);
+      createAndSaveUrl(
+        urlWOprotocol[1],
+        req.body.short_url,
+        function (err, data) {
+          clearTimeout(t);
+          if (err) {
+            return next(err);
+          }
+          if (!data) {
+            console.log("Missing `done()` argument");
+            return next({ message: "Missing callback argument" });
+          }
+          UrlPair.findById(data._id, function (err, urlp) {
+            if (err) {
+              return next(err);
+            }
+            res.json(urlp);
+            pers.remove();
+          });
+        }
+      );
+
+      // respond
       res.json({
         original_url: req.body.original_url,
         short_url: req.body.short_url,
@@ -44,11 +109,9 @@ app.post("/api/shorturl", (req, res) => {
   });
 });
 
+// retrieve url pair and redirect
 app.get("/api/shorturl/:shorturl", (req, res) => {
-  res.json({
-    original_url: req.body.original_url,
-    short_url: req.body.short_url,
-  });
+  // TODO
 });
 
 // you can use the function dns.lookup(host, cb) from the dns core module to verify a submitted URL.
