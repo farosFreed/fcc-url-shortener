c59c4638e0d2ed1e538df20c9b51f6f24295d950
farosFreed <jessica.divers@gmail.com>
2023-02-18 16:21:43 -0700

    clean up approach

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
 // enable body parsing
 const bodyParser = require("body-parser");
 // enable dns lookup
-const dns = require("node:dns");
+const dns = require("dns");
+const url = require("url");
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -17,27 +17,6 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-
-app.use(cors());
-
-app.use("/public", express.static(`${process.cwd()}/public`));
-
-app.get("/", function (req, res) {
-  res.sendFile(process.cwd() + "/views/index.html");
-});
-
-// Your first API endpoint
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
-});
-
-// body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/api/shorturl", bodyParser.urlencoded({ extended: false }));
-
-// timeout
-const TIMEOUT = 10000;
-
 // setup url pair mongoose scheme
 let urlSchema = new mongoose.Schema({
   original_url: {
@@ -49,107 +28,64 @@ let urlSchema = new mongoose.Schema({
     required: true,
   },
 });
+const Url = mongoose.model("urlPair", urlSchema);
 
-let UrlPair = mongoose.model("urlPair", urlSchema);
+app.use(cors());
 
-// create and save helper function
-const createAndSaveUrl = (original, short, done) => {
-  let newpair = new UrlPair({
-    original_url: original,
-    short_url: short,
-  });
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+// json responses
+// app.use(express.json());
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-  newpair.save((err, data) => {
-    if (err) {
-      console.log(err);
-    }
-    done(null, data);
-  });
-};
-
-// save url pair to mongoose database
-app.post("/api/shorturl", (req, res, next) => {
-  // check if original url is valid
-  let urlWOprotocol = req.body.original_url.match(
-    /https?:\/\/([a-z, A-Z, 0-9, ., -]*)\/[a-z, A-Z, \?, =, 0-9, \/, ., -]*/
-  );
-  console.log("match: " + urlWOprotocol);
-  if (urlWOprotocol === null) {
-    return next(
-      res.json({
-        error: "invalid url",
-      })
-    );
-  } else {
-    dns.lookup(urlWOprotocol[1], (err, data) => {
-      if (err) {
-        return next(
-          res.json({
-            error: "invalid url",
-          })
-        );
-      } else {
-        console.log("create and save");
-        // save to mongoose
-        // in case of incorrect function use wait timeout then respond
-        let t = setTimeout(() => {
-          next({ message: "timeout" });
-        }, TIMEOUT);
-        createAndSaveUrl(
-          req.body.original_url,
-          req.body.short_url,
-          function (err, data) {
-            clearTimeout(t);
-            if (err) {
-              return next(err);
-            }
-            if (!data) {
-              console.log("Missing `done()` argument");
-              return next({ message: "Missing callback argument" });
-            }
-            UrlPair.findById(data._id, function (err, urlp) {
-              if (err) {
-                return next(err);
-              }
-              // respond
-              return next(
-                res.json({
-                  original_url: req.body.original_url,
-                  short_url: req.body.short_url,
-                })
-              );
-            });
-          }
-        );
-      }
-    });
-  }
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-const findUrlPairByShortUrl = (short, done) => {
-  UrlPair.findOne({ short_url: short }, (err, data) => {
-    done(null, data);
-  });
-};
+// body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use("/api/shorturl", bodyParser.urlencoded({ extended: false }));
 
-// retrieve url pair and redirect
-app.get("/api/shorturl/:shorturl", (req, res) => {
-  findUrlPairByShortUrl(req.params.shorturl, function (err, data) {
-    let t = setTimeout(() => {
-      next({ message: "timeout" });
-    }, TIMEOUT);
-    clearTimeout(t);
-    if (err) {
-      console.log(err);
-    }
-    if (!data) {
-      console.log("missing data");
+// Your first API endpoint
+app.post("/api/shorturl", (req, res) => {
+  let fixedUrl = url.parse(req.body.url).hostname;
+  dns.lookup(fixedUrl, async (err, address) => {
+    if (err || fixedUrl === null) {
+      res.json({ error: "invalid url" });
     } else {
-      console.log(data);
-      let redirectUrl = data.original_url; // "https://" +
-      res.redirect(redirectUrl);
+      const foundDoc = await Url.findOne({ original_url: req.body.url });
+      if (!foundDoc) {
+        let lastDoc = await Url.find().sort({ short_url: -1 }).limit(1);
+        let newUrl = {};
+        if (lastDoc.length === 0) {
+          newUrl = { original_url: req.body.url, short_url: 1 };
+        } else {
+          let lastShort = lastDoc[0].short_url;
+          newUrl = { original_url: req.body.url, short_url: lastShort * 1 + 1 };
+        }
+        const result = await Url.create(newUrl);
+        res.json({
+          original_url: result.original_url,
+          short_url: result.short_url,
+        });
+      } else {
+        res.json({
+          original_url: foundDoc.original_url,
+          short_url: foundDoc.short_url,
+        });
+      }
     }
   });
+});
+
+app.get("/api/shorturl/:index", async (req, res) => {
+  const index = req.params.index * 1;
+  const foundDoc = await Url.findOne({ short_url: index });
+  if (!foundDoc) {
+    res.json({ error: "invalid url" });
+  } else {
+    res.redirect(foundDoc.original_url);
+  }
 });
 
 app.listen(port, function () {
